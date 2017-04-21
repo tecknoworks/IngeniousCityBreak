55ff3ec68458a3665fad3f6dd9ea2ff0dd4d7863
Marina Baban <marina.baban@tecknoworks.com>
2017-04-21 11:00:30 +0300

    CityDto, CountryDto, RouteDto, TouristAttractionDto

---
+++ BusinessLayer/Dto/TouristAttractionDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Dto
+{
+    public class TouristAttractionDto
+    {
+        public int? IdTouristAttraction { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public float PriceChild { get; set; }
+
+        public float PriceAdult { get; set; }
+
+        public DateTime Schedule { get; set; }
+
+        public City City { get; set; }
+
+    }
+}
