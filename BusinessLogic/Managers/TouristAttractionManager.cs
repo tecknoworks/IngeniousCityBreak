097716f459b45e691a250763c09e5f3f66b4f088
Loredana Petrea <loredana.petrea@tecknoworks.com>
2017-04-27 08:58:08 +0300

    Create TouristAttractionManeger

---
+++ BusinessLogic/Managers/TouristAttractionManager.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer;
+using BusinessLayer.Dto;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class TouristAttractionManager : TouristAttraction
+    {
+        private IDbRepository<TouristAttraction> _touristAttractionRepository { get; set; }
+        //protected IDbRepository<UserDetails> UserDetailsRepository { get; set; }
+
+        public TouristAttractionManager(IDbRepository<TouristAttraction> touristAttractionRepository)
+        {
+            _touristAttractionRepository = touristAttractionRepository;
+        }
+
+        public void Insert(TouristAttractionDto touristAttractionDto)
+        {
+            //var entity = Mapper.FromDto(touristAttractionDto);
+            //UserDetailsRepository.Insert(entity);
+            //UserDetailsRepository.Save();
+        }
+        //public IEnumerable<UserDetailsDto> GetAll()
+        //{
+        //    var result = UserDetailsRepository.GetAll().ToUserDetailsDto();
+        //    var result = Mapper.ToUserDetailsDto(UserDetailsRepository.GetAll());
+        //    return result;
+        //}
+
+    }
+}
---
+++ Contracts/Managers/ITouristAttraction.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface ITuristAttraction
+    {
+        void Insert(TouristAttractionDto turistAttractionDto);
+
+        IEnumerable<TouristAttractionDto> GetAll();
+    }
+}
