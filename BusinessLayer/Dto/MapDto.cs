5e6285ae88af30137a700b70c06ec58d2f347c35
Loredana Petrea <loredana.petrea@tecknoworks.com>
2017-05-11 12:31:55 +0300

    Map LinkedList

---
+++ BusinessLayer/Dto/MapDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class MapDto
+    {
+        public int? IdMarker { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
---
+++ IngeniousCityBreakWeb/Scripts/App/Map/map.controller.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=map.controller.js.map
