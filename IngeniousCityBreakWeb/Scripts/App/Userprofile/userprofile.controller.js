9e4403daef6befc488351a8e8406444a9cbdb172
Marina Baban <marina.baban@tecknoworks.com>
2017-05-25 11:22:05 +0300

    contact commit

--- IngeniousCityBreakWeb/Scripts/App/Userprofile/userprofile.controller.js
+++ IngeniousCityBreakWeb/Scripts/App/Userprofile/userprofile.controller.js
@@ -10,12 +10,65 @@ var UserProfileController = (function (_super) {
         this.HttpService = $http;
         this.UPModel = new UserProfileModel();
         this.Initialize();
+        var self = this;
         if (this.currentUser == null)
             this.IWindowService.location.href = '/index.html#!/home';
+        var config = {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": this.currentUser.TokenType + this.currentUser.AccessToken,
+            }
+        };
+        this.HttpService.get("api/UserDetails/?email=" + this.currentUser.UserName, config).then(function (response) {
+            console.log(response.data);
+            self.UPModel.Email = response.data.Email;
+            self.UPModel.FirstName = response.data.FirstName;
+            self.UPModel.LastName = response.data.LastName;
+            self.UPModel.Country = response.data.Country;
+            self.UPModel.City = response.data.City;
+            self.UPModel.Address = response.data.Address;
+            self.UPModel.Telephone = response.data.Telephone;
+        }).catch(function (err) {
+            console.log(err);
+        });
     }
     UserProfileController.prototype.SavePasswordClick = function () {
     };
     UserProfileController.prototype.SaveClick = function () {
+        var self = this;
+        self.UPModel.ErrorAlert = false;
+        debugger;
+        if (self.UPModel.Email == null || self.UPModel.FirstName == null || self.UPModel.LastName == null || self.UPModel.Country == null || self.UPModel.City == null || self.UPModel.Address == null || self.UPModel.Telephone == null) {
+            self.UPModel.ErrorMessage = "You have to complete all fields!";
+            self.UPModel.ErrorAlert = true;
+            return;
+        }
+        debugger;
+        var config = {
+            headers: {
+                "dataType": "json",
+                "contentType": "application/json",
+                "Authorization": self.currentUser.TokenType + self.currentUser.AccessToken,
+            }
+        };
+        debugger;
+        this.HttpService.post('api/UserDetails', {
+            "Id": 5,
+            "Email": self.UPModel.Email,
+            "Username": self.UPModel.Email,
+            "FirstName": self.UPModel.FirstName,
+            "LastName": self.UPModel.LastName,
+            "Country": self.UPModel.Country,
+            "City": self.UPModel.City,
+            "Address": self.UPModel.Address,
+            "Telephone": self.UPModel.Telephone,
+        }).then(function (response) {
+            self.UPModel.ErrorMessage = "";
+            console.log(response.data);
+        }).catch(function (response) {
+            console.log(response.data);
+            self.UPModel.ErrorMessage = response.data.Message;
+        });
     };
     UserProfileController.prototype.Initialize = function () {
         var _this = this;
